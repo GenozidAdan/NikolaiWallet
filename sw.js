ab3d5504d3c0e65cc0b233d1d18ee8037a5ae1fa
Luis Angel Meza Adan <113572028+GenozidAdan@users.noreply.github.com>
2026-05-23 10:28:58 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,59 @@
-const CACHE_NAME = "kaz-money-v1";
+const CACHE_NAME = "kaz-money-v2";
 
 const urlsToCache = [
   "./",
   "./index.html",
-  "./movimientos.js",
   "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
 
+  self.skipWaiting();
+
   event.waitUntil(
+
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+
   );
+
+});
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys.map(key => {
+
+          if(key !== CACHE_NAME){
+            return caches.delete(key);
+          }
+
+        })
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
 
 });
 
 self.addEventListener("fetch", event => {
 
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+
   );
 
 });
